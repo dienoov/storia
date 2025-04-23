b3f7689970d8c572a95eb9c305b05d14c9d9bfda
dienoov <alvinodien@yahoo.co.id>
2025-04-23 10:11:27 +0700

    chore: add story model

---
+++ lib/models/story.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+Story storyFromJson(String str) => Story.fromJson(json.decode(str));
+
+String storyToJson(Story data) => json.encode(data.toJson());
+
+class Story {
+  final String id;
+  final String name;
+  final String description;
+  final String photoUrl;
+  final DateTime createdAt;
+  final double? lat;
+  final double? lon;
+
+  Story({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.photoUrl,
+    required this.createdAt,
+    required this.lat,
+    required this.lon,
+  });
+
+  factory Story.fromJson(Map<String, dynamic> json) => Story(
+    id: json["id"],
+    name: json["name"],
+    description: json["description"],
+    photoUrl: json["photoUrl"],
+    createdAt: DateTime.parse(json["createdAt"]),
+    lat: json["lat"]?.toDouble(),
+    lon: json["lon"]?.toDouble(),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "description": description,
+    "photoUrl": photoUrl,
+    "createdAt": createdAt.toIso8601String(),
+    "lat": lat,
+    "lon": lon,
+  };
+}
