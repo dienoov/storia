8ff2fc8bf1b624ccb27e659d3d7e3d6bfb1888a4
dienoov <alvinodien@yahoo.co.id>
2025-04-23 15:21:20 +0700

    chore: add route parser

---
+++ lib/router/parser.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:storia/router/routes.dart';
+
+class StoriaRouteParser extends RouteInformationParser<StoriaRoute> {
+  @override
+  Future<StoriaRoute> parseRouteInformation(
+    RouteInformation routeInformation,
+  ) async {
+    final uri = Uri.parse(routeInformation.uri.toString());
+
+    if (uri.pathSegments.isNotEmpty) {
+      switch (uri.pathSegments[0].toLowerCase()) {
+        case 'login':
+          return LoginRoute();
+        case 'register':
+          return RegisterRoute();
+        case 'story':
+          if (uri.pathSegments.length == 2) {
+            return StoryRoute(uri.pathSegments[1]);
+          }
+      }
+    }
+
+    return HomeRoute();
+  }
+
+  @override
+  RouteInformation? restoreRouteInformation(StoriaRoute configuration) {
+    return switch (configuration) {
+      LoginRoute() => RouteInformation(uri: Uri.parse("/login")),
+      RegisterRoute() => RouteInformation(uri: Uri.parse("/register")),
+      StoryRoute() => RouteInformation(
+        uri: Uri.parse("/story/${configuration.id}"),
+      ),
+      _ => RouteInformation(uri: Uri.parse("/")),
+    };
+  }
+}
---
+++ lib/router/routes.dart
@@ -0,0 +1,13 @@
+sealed class StoriaRoute {}
+
+final class LoginRoute extends StoriaRoute {}
+
+final class RegisterRoute extends StoriaRoute {}
+
+final class HomeRoute extends StoriaRoute {}
+
+final class StoryRoute extends StoriaRoute {
+  final String? id;
+
+  StoryRoute(this.id);
+}
